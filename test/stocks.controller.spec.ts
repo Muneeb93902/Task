dd14b5e91b5604a5ef51870532a7ac7fd4d7b4ed
Muneeb93902 <muneeb9390@yahoo.com>
2022-08-21 10:14:01 +0500

    add new tests

--- test/stocks.controller.spec.ts
+++ test/stocks.controller.spec.ts
@@ -3,10 +3,17 @@ import { expect } from 'chai';
 
 describe('stockController functions', () => {
     const stockData = [{"sku":"LTV719449/39/39","stock":8525},{"sku":"CLQ274846/07/46","stock":8414}];
+  it('getFilteredStockDataBySKU stock found', () => {
     const sku = "LTV719449/39/39";
-  it('getFilteredStockDataBySKU', () => {
     const result = stockController.getFilteredStockDataBySKU(stockData, sku);
     expect(typeof result).to.equal('object');
     expect(result.length).to.equal(1);
+  });
+
+  it('getFilteredStockDataBySKU stock not found', () => {
+    const sku = "LTV719449/39/39/33/33/333";
+    const result = stockController.getFilteredStockDataBySKU(stockData, sku);
+    expect(typeof result).to.equal('object');
+    expect(result.length).to.equal(0);
   });
 });
--- test/transactions.controller.spec.ts
+++ test/transactions.controller.spec.ts
@@ -3,11 +3,18 @@ import { expect } from 'chai';
 
 describe('transactionController functions', () => {
     const transactionData = [{"sku":"KED089097/68/09","type":"order","qty":8}];
+  it('getFilteredTransactionDataBySKU stock found', () => {
     const sku = "KED089097/68/09";
-  it('getFilteredTransactionDataBySKU', () => {
     const result = transactionController.getFilteredTransactionDataBySKU(transactionData, sku);
     expect(typeof result).to.equal('object');
     expect(result.length).to.equal(1);
+  });
+
+  it('getFilteredTransactionDataBySKU stock not found', () => {
+    const sku = "KED089097/68/09//99/33";
+    const result = transactionController.getFilteredTransactionDataBySKU(transactionData, sku);
+    expect(typeof result).to.equal('object');
+    expect(result.length).to.equal(0);
   });
 
   it('getTypeTransactionCountBySKU', () => {
